61d7381aa326ca6aab93c3c83bb56cb45c6adc52
frmauro <frmauro8@gmail.com>
2021-11-29 17:15:33 -0300

    many changes

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 public class Product {
     public int Id { get; set; }
-    public string Description { get; set; }
+    [Required]
+    public string? Description { get; set; }
+    [Required]
+    public int Amount { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
+    public string? Status { get; set; }
+
+    
 }
