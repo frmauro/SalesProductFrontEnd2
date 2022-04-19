a886eeaa40647bbf5ee71d175f4af7e688d67f2d
frmauro <frmauro8@gmail.com>
2022-04-19 14:34:39 -0300

    many changes

---
+++ deploytst.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+########################################################
+
+## Shell Script to Build Docker Image and run.   
+
+########################################################
+
+
+result=$( docker images -q webproduct )
+if [[ -n "$result" ]]; then
+echo "image exists"
+ docker rmi -f webproduct
+else
+echo "No such image"
+fi
+
+echo "build the docker image"
+echo "built docker images and proceeding to delete existing container"
+
+result=$( docker ps -q -f name=webproduct )
+if [[ $? -eq 0 ]]; then
+echo "Container exists"
+ docker container rm -f webproduct
+echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+cp -a /home/francisco/estudos/azuredevops/myagent/_work/14/s/.  /home/francisco/estudos/azuredevops/myagent/_work/r13/a/
+
+docker build -t webproduct .
+
+echo "built docker images and proceeding to delete existing container"
+echo "Deploying the updated container"
+
+docker run -d -p 8080:80 --link salesproductapi webproduct
+
+echo "Deploying the container"
